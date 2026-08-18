e8711c8de690a7648fae10ee530048218d415704
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-08-17 21:27:51 -0300

    update funcao sleep

--- src/functions/sleep.js
+++ src/functions/sleep.js
@@ -1,5 +1,12 @@
-import leia from 'readline-sync';
+import leia, { keyInSelect } from 'readline-sync';
+import { jogador } from './player';
 
 export function descansar() {
-    
+    let descansar = keyInSelect(["Sim", "Nao"]);
+
+    if(descansar === 0){
+        jogador.acoesPerDia = 3;
+        jogador.dias++;
+        jogador.energia += 30;
+    }
 }
