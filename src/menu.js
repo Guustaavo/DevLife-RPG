47cbf11321138c9d686e7f73f32fd856771012a5
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-08-20 18:55:59 -0300

    denovo

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,7 @@
 import leia from 'readline-sync';
 import { jogador, status } from './functions/player.js';
+import { trabalhar } from './functions/work.js';
+import { descansar } from './functions/sleep.js';
 
 let estaNoJogo = true;
 let nome = leia.question("Digite o nome do jogador: ");
@@ -16,4 +18,12 @@ while(estaNoJogo === true){
    console.log("energia: " + jogador.energia)
    console.log("dinheiro: " + jogador.dinheiro)
    
+   let opcao = leia.keyInSelect(["trabalhar","descansar"],"escolha sua proxima ação:")
+
+   if(opcao === 0){
+      trabalhar()
+   }
+   else if(opcao === 1){
+      descansar()
+   }
 }
