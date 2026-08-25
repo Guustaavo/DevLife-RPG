a3f41cbd617fd3c5f8d5e2a1973ea288fe513958
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-08-24 21:44:23 -0300

    cargos

---
+++ src/containers/nivelXP.js
@@ -0,0 +1,29 @@
+import { jogador } from "./player.js";
+
+export const cargos = {
+    Estagiario: {
+        gastoEnergia: 20,
+        salario: 1800,
+        xpNecessario: 300
+    },
+    Junior: {
+        gastoEnergia: 25,
+        salario: 2400,
+        xpNecessario: 750
+    },
+    Pleno: {
+        gastoEnergia: 30,
+        salario: 5000,
+        xpNecessario: 1200
+    },
+    Senior: {
+        gastoEnergia: 35,
+        salario: 10000,
+        xpNecessario: 2000
+    },
+    TechLead: {
+        gastoEnergia: 50,
+        salario: 18000,
+        xpNecessario: 2800
+    }
+}
