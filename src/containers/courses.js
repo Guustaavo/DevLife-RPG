bd6aa761dbd24ba790a89ed3c7c3d3adc011bf99
Gustavo <gustavo.guimaraes2148@gmail.com>
2026-08-25 19:41:17 -0300

    cursos

---
+++ src/containers/courses.js
@@ -0,0 +1,39 @@
+import leia from 'readline-sync';
+
+export const lojaCursos = [
+    {
+        nivel: 1,
+        nome: "Introdução à Lógica de Programação",
+        preco: 29.90,
+        bonusConhecimento: 2,
+        tempoConclusao: 7
+    },
+    {
+        nivel: 2,
+        nome: "Bootcamp Desenvolvedor Web Front-End",
+        preco: 1500.00,
+        bonusConhecimento: 10,
+        tempoConclusaoMs: 15
+    },
+    {
+        nivel: 3,
+        nome: "Tecnólogo em Análise e Dev. de Sistemas",
+        preco: 4200.00, // Valor total do curso
+        bonusConhecimento: 25,
+        tempoConclusaoMs: 60
+    },
+    {
+        nivel: 4,
+        nome: "Especialização em Arquitetura de Software e Cloud",
+        preco: 4800.00,
+        bonusConhecimento: 50,
+        tempoConclusaoMs: 180
+    },
+    {
+        nivel: 5,
+        nome: "Mestrado em Ciência da Computação e IA",
+        preco: 45000.00,
+        bonusConhecimento: 100,
+        tempoConclusaoMs: 365
+    }
+];
