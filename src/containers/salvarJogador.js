5c37c20d9fb1a6bfe2556007f45ded5caf0e28e9
Nycollas Belchior <nyck2102@gmail.com>
2026-08-27 01:02:05 -0300

    data agora esta configurada para America/sp

--- src/containers/salvarJogador.js
+++ src/containers/salvarJogador.js
@@ -7,47 +7,64 @@ export function salvarJogador(jogador) {
     let jogadores = [];
 
     if (fs.existsSync(caminho)) {
+
         const dados = fs.readFileSync(caminho, "utf-8");
 
         if (dados.trim() !== "") {
+
             jogadores = JSON.parse(dados);
+
         }
+
     }
 
-    // Se ainda não possui ID, cria um novo
     if (!jogador.id) {
 
         if (jogadores.length === 0) {
+
             jogador.id = 1;
+
         } else {
+
             const maiorId = Math.max(
                 ...jogadores.map(jogador => jogador.id)
             );
 
             jogador.id = maiorId + 1;
+
         }
 
-        jogador.dataCriacao = new Date().toISOString();
+        jogador.dataCriacao = new Date().toLocaleString("pt-BR", {
+            timeZone: "America/Sao_Paulo"
+        });
+
+        jogador.dataAtualizacao = new Date().toLocaleString("pt-BR", {
+            timeZone: "America/Sao_Paulo"
+        });
 
         jogadores.push(jogador);
 
     } else {
 
-        // Procura a carreira pelo ID
         const indice = jogadores.findIndex(
             j => j.id === jogador.id
         );
 
-        jogador.dataAtualizacao = new Date().toISOString();
+        jogador.dataAtualizacao = new Date().toLocaleString("pt-BR", {
+            timeZone: "America/Sao_Paulo"
+        });
 
         if (indice !== -1) {
-            jogadores[indice] = jogador;
-        } else {
-            jogadores.push(jogador);
-        }
-    }
 
-    jogador.dataAtualizacao = new Date().toISOString();
+            jogadores[indice] = jogador;
+
+        } else {
+
+            jogadores.push(jogador);
+
+        }
+
+    }
 
     fs.writeFileSync(
         caminho,
