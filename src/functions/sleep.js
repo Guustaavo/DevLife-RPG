c1cd5a5be9c9c9297cade6a83498a1f01c263059
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-08-27 20:55:16 -0300

    jogo totalmente FEITO

--- src/functions/sleep.js
+++ src/functions/sleep.js
@@ -1,24 +1,19 @@
-import leia from 'readline-sync';
 import { jogador } from '../containers/player.js';
 import { atualizarCurso } from "../containers/courses.js";
 import { salvarJogador } from "../containers/salvarJogador.js";
 
-
 export async function descansar() {
-    let descansar = leia.keyInSelect(["\nSim", "Nao"], "Você deseja descansar?");
+    console.log("\n😴 Você descansou.");
+    
+    jogador.dias++;
+    atualizarCurso();
 
-    if (descansar === 0) {
-        console.log("Você descansou.");
-        jogador.dias++;
-        atualizarCurso();
-        if (jogador.energia >= (jogador.energiaMaxima - jogador.gastoEnergia)) {
-            jogador.energia = jogador.energiaMaxima;
-        }
-        else if (jogador.energia <= jogador.energiaMaxima) {
-            jogador.energia += 30;
-        }
-        salvarJogador(jogador);
-    } else {
-        console.log("Você não descansou.");
+    if (jogador.energia >= (jogador.energiaMaxima - jogador.gastoEnergia)) {
+        jogador.energia = jogador.energiaMaxima;
     }
+    else if (jogador.energia <= jogador.energiaMaxima) {
+        jogador.energia += 30;
+    }
+
+    salvarJogador(jogador);
 }
--- src/functions/work.js
+++ src/functions/work.js
@@ -1,14 +1,11 @@
-import leia from 'readline-sync';
 import { jogador } from '../containers/player.js';
 import { salvarJogador } from '../containers/salvarJogador.js';
 import { atualizarCurso } from "../containers/courses.js";
 
-
 export async function trabalhar() {
-    let trabalhar = leia.keyInSelect(["\nSim", "Nao"], "Você deseja trabalhar?");
 
-    if (trabalhar === 0 && jogador.energia >= jogador.gastoEnergia) {
-        console.log("Você trabalhou e ganhou: R$" + jogador.salario);
+    if (jogador.energia >= jogador.gastoEnergia) {
+        console.log("💼 Você trabalhou e ganhou: R$" + jogador.salario);
         jogador.dias++;
         atualizarCurso();
         jogador.dinheiro += jogador.salario;
@@ -16,11 +13,8 @@ export async function trabalhar() {
         jogador.xp += (jogador.ganhoXp * jogador.bonusConhecimento);
         salvarJogador(jogador);
     }
-    else if (trabalhar === 0 && jogador.energia < jogador.gastoEnergia) {
-        console.log("Sem energia o suficiente!");
-        console.log("Descanse para poder trabalhar novamente.");
-    }
     else {
-        console.log("Você não trabalhou!");
+        console.log("❌ Sem energia o suficiente!");
+        console.log("😴 Descanse para poder trabalhar novamente.");
     }
 }
