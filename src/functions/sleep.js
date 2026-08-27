f3fc2008e839f493bf9cbf2283be13b25afd6db9
NYCOLLAS CORDEIRO NORONHA BELCHIOR <nycollas_belchior@estudante.sesisenai.org.br>
2026-08-27 20:03:59 -0300

    tentando ajeitar algo

--- src/functions/sleep.js
+++ src/functions/sleep.js
@@ -1,25 +1,22 @@
+import leia from 'readline-sync';
 import { jogador } from '../containers/player.js';
 import { cargos, Niveis } from '../containers/nivelXP.js';
-import { salvarJogador } from "../containers/salvarJogador.js";
-import { menuSeta } from "./menuSeta.js";
+import { atualizarCurso } from "../containers/courses.js";
 
 
-export async function descansar() {
-    let descansar = await menuSeta(
-        "😴 Você deseja descansar?",
-        ["Sim", "Não"]
-    );
+export function descansar() {
+    let descansar = leia.keyInSelect(["\nSim", "Nao"], "Você deseja descansar?");
 
     if (descansar === 0) {
         console.log("Você descansou.");
         jogador.dias++;
+        atualizarCurso();
         if (jogador.energia >= (jogador.energiaMaxima - jogador.gastoEnergia)) {
             jogador.energia = jogador.energiaMaxima;
         }
         else if (jogador.energia <= jogador.energiaMaxima) {
             jogador.energia += 30;
         }
-        salvarJogador(jogador);
     } else {
         console.log("Você não descansou.");
     }
--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,19 @@
 import leia from 'readline-sync';
+
 import { jogador, status } from './containers/player.js';
+
 import { trabalhar } from './functions/work.js';
+
 import { descansar } from './functions/sleep.js';
+
 import { loja } from './functions/buy.js';
+
 import { cargos } from './containers/nivelXP.js';
+
 import { cadastro } from "./database/cadastro.js";
+
 import { login } from "./database/login.js";
+
 import { menuSeta } from "./functions/menuSeta.js";
 
 
@@ -13,25 +21,31 @@ let entrada = await menuSeta(
     "🎮 Dev. Life Game RPG",
     [
         "📝 Cadastrar",
-        "🔑 Login",
+        "🔑 Continuar carreira",
         "🚪 Sair"
     ]
 );
 
 
 if (entrada === 0) {
+
     cadastro();
+
 }
 else if (entrada === 1) {
-    const entrou = login();
+
+    const entrou = await login();
 
     if (!entrou) {
         process.exit();
     }
+
 }
 else {
+
     console.log("Jogo encerrado.");
     process.exit();
+
 }
 
 
@@ -43,6 +57,7 @@ while (estaNoJogo === true) {
     header += "  🎮 Dev. Life Game RPG  \n";
     header += "=======================\n";
     header += "\n👤 Jogador: " + jogador.nome;
+    header += "\n🆔 ID: " + jogador.id;
     header += "\n⚡ Energia: " + jogador.energia;
     header += "\n💰 Dinheiro: " + jogador.dinheiro;
     header += "\n📅 Dias: " + jogador.dias;
@@ -64,29 +79,43 @@ while (estaNoJogo === true) {
 
 
     if (acao === 0) {
+
         status();
+
     }
 
     else if (acao === 1) {
+
         await trabalhar();
+
     }
 
     else if (acao === 2) {
+
         await descansar();
+
     }
 
     else if (acao === 3) {
+
         await loja();
+
     }
 
     else if (acao === 4) {
+
         await cargos();
+
         let retornar = leia.keyInSelect(["Retornar"], "Deseja retornar ao menu principal?");
+
     }
 
     else if (acao === 5 || acao === -1) {
+
         console.log("Jogo encerrado.");
+
         estaNoJogo = false;
+
     }
 
 }
